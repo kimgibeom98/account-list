c7244b266cb0fdb43e5f6518a79fb76e2d438faf
PC <kim000183@naver.com>
2022-09-19 21:42:01 +0900

    수정 기능 완료

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ const searchType = document.getElementById('search-type');
 const list = document.getElementById('target');
 let data;
 function getData() {
+  // fetch("http://localhost:3000/accoounts")
+  // .then((response) => response.json())
+  // .then((data) => console.log(data));
+
   dataJseon.open('GET', requestURL);
   dataJseon.send();
   dataJseon.responseType = 'json';
@@ -25,7 +29,7 @@ function getData() {
       if (data.length - 1 === i) {
         targetCount.value = Number(data[i].id) + 1;
       }
-      targetList.innerHTML += `<tr><td data-index=${data[i].id}>${j}</td><td class="target-name${i}"><input type="text" readonly value="${data[i].name}"></td><td><input type="text" readonly value="${data[i].age}"></td><td><input type="text" readonly value="${data[i].job}"></td><td><div class="button-box"><span><input type="text" readonly value="${data[i].email}"></span><div><button class="correction-data" type="button">수정</button><button onclick="findName(${i});" type="button" class="del-btn">삭제</button></div></div></td></tr>`;
+      targetList.innerHTML += `<tr><td data-index=${data[i].id}>${j}</td><td class="target-name${i}"><span class="view-data">${data[i].name}</span><input class="correction-input" id="up-name" type="text" readonly value="${data[i].name}"></td><td><span class="view-data">${data[i].age}</span><input class="correction-input" id="up-age" type="text" readonly value="${data[i].age}"></td><td><span class="view-data">${data[i].job}</span><input class="correction-input" id="up-job" type="text" readonly value="${data[i].job}"></td><td><div class="button-box"><span><span class="view-data">${data[i].email}</span><input class="correction-input" id="up-email" type="text" readonly value="${data[i].email}"></span><div><button class="correction-data" type="button">수정</button><button class="up-data" type="button">완료</button><button onclick="findName(${i});" type="button" class="del-btn">삭제</button></div></div></td></tr>`;
       j++;
     }
     countTable.innerHTML = listCount.rows.length;
@@ -52,15 +56,45 @@ function postData(event) {
     const val = searchInput.value;
     showList(val)
   }else if(event.target.getAttribute('class') === 'correction-data'){
-    event.target.innerText = "완료"
+    console.log(event.target)
+    event.target.style.display = "none";
+    event.target.nextSibling.style.display = "block";
+    const updateTr = event.target.parentNode.parentNode.parentNode.parentNode; 
+    const updateInput = updateTr.querySelectorAll('input');
+    const viewSpan = updateTr.querySelectorAll('.view-data');
+    console.log(viewSpan)
+    // viewSpan.style.display = "none"
+    for(let i = 0; i < updateInput.length; i++){
+      updateInput[i].readOnly  = false;
+      updateInput[i].style.border ='1px solid #888',
+      updateInput[i].style.background ='#fff'
+    }
+  }else if(event.target.getAttribute('class') === 'up-data'){
+    const updateTr = event.target.parentNode.parentNode.parentNode.parentNode
+    const patchNum = updateTr.firstChild.dataset.index;
+    const upName = updateTr.querySelector('#up-name');
+    const upAge = updateTr.querySelector('#up-age');
+    const upJob = updateTr.querySelector('#up-job');
+    const upemail = updateTr.querySelector('#up-email');
+    event.target.style.display = "none";
+    event.target.previousSibling .style.display = "block";
+    dataJseon.open('PATCH', `${requestURL}/${Number(patchNum)}`);
+    dataJseon.setRequestHeader('content-type', 'application/json');
+    dataJseon.send(JSON.stringify({id: Number(targetCount.value), name: upName.value, age: Number(upAge.value), job: upJob.value, email: upemail.value }));
+    dataJseon.onload = () => {
+      targetList.innerHTML = '';
+      getData();
+      alert("수정이 완료되었습니다.");
+    }
   }
 }
 
 function findName(targetNum) {
   const delName = document.querySelector(`.target-name${targetNum}`);
   const delId = delName.previousSibling.dataset.index
+  const delNameval = document.querySelector(`.target-name${targetNum} > input`);
   delList.style.display = 'block';
-  delList.innerHTML = `<p>${delName.innerText} 님을 삭제하시겠습니까?</p><div><button type="button" onclick="deleteData(${delId})" class="y-btn">예</button><button type="button" class="n-btn">아니요</button></div>`
+  delList.innerHTML = `<p>${delNameval.value} 님을 삭제하시겠습니까?</p><div><button type="button" onclick="deleteData(${delId})" class="y-btn">예</button><button type="button" class="n-btn">아니요</button></div>`
   const delPop = document.querySelector('.n-btn');
   delPop.onclick = function () {
     delList.style.display = 'none';
@@ -76,8 +110,8 @@ function deleteData(num) {
     getData();
   }
 }
-function showList(val) {
 
+function showList(val) {
   list.innerHTML = '';
   dataJseon.open('GET', requestURL);
   dataJseon.send();
@@ -86,22 +120,22 @@ function showList(val) {
   data = dataJseon.response;
   let k = 1;
   for (let i = 0; i < data.length; i++) {
-    const innerElement = `<tr><td data-index=${data[i].id}>${k}</td><td class="target-name${i}"><input type="text" readonly value="${data[i].name}"></td><td><input type="text" readonly value="${data[i].age}"></td><td><input type="text" readonly value="${data[i].job}"></td><td><div class="button-box"><span><input type="text" readonly value="${data[i].email}"></span><div><button class="correction-data" type="button">수정</button><button onclick="findName(${i});" type="button" class="del-btn">삭제</button></div></div></td></tr>`
     if (data.length - 1 === i) {
       targetCount.value = Number(data[i].id) + 1;
     }
+    const innerElement = `<tr><td data-index=${data[i].id}>${k}</td><td class="target-name${i}"><span class="view-data">${data[i].name}</span><input class="correction-input" id="up-name" type="text" readonly value="${data[i].name}"></td><td><span class="view-data">${data[i].age}</span><input class="correction-input" id="up-age" type="text" readonly value="${data[i].age}"></td><td><span class="view-data">${data[i].job}</span><input class="correction-input" id="up-job" type="text" readonly value="${data[i].job}"></td><td><div class="button-box"><span><span class="view-data">${data[i].email}</span><input class="correction-input" id="up-email" type="text" readonly value="${data[i].email}"></span><div><button class="correction-data" type="button">수정</button><button class="up-data" type="button">완료</button><button onclick="findName(${i});" type="button" class="del-btn">삭제</button></div></div></td></tr>`;
       if(searchType.value === ""){
         alert("분류를 선택하세요");
         getData();
         break;
       }else if(searchType.value === "name"){
         if(data[i].name.includes(val)){
-          targetList.innerHTML += innerElement
+          targetList.innerHTML +=  innerElement
           k++;
         }
       }else if(searchType.value === "email"){
         if(data[i].email.includes(val)){
-          targetList.innerHTML += innerElement
+          targetList.innerHTML +=  innerElement
           k++;
         }
       }else if(searchType.value === "age"){
@@ -111,7 +145,7 @@ function showList(val) {
         }
       }else if(searchType.value === "job"){
         if(data[i].job === val){
-          targetList.innerHTML += innerElement
+          targetList.innerHTML +=  innerElement
           k++;
         }
       }
