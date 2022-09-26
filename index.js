a8a25211fef0ddfddc85ea5507f9d369922860c7
PC <kim000183@naver.com>
2022-09-26 22:06:30 +0900

    기능에러 수정중

--- index.js
+++ index.js
@@ -149,17 +149,21 @@ async function deleteData(num) {
 }
 
 async function showList(val) {
-  list.innerHTML = '';
-  try{
-    const data = await fetchOption(requestURL);
-    timeOut();
-    const post = await data.json()
-    await serarchFetch(post, val);
-  }catch(err){
-    alert(err);
-    countTable.innerHTML = listCount.rows.length;
+  if(searchType.value === '' || searchType.value === '선택'){
+    alert("분류를 선택하세요"); 
+  }else{
+    list.innerHTML = '';
+    try{
+      const data = await fetchOption(requestURL);
+      timeOut();
+      const post = await data.json()
+      await serarchFetch(post, val);
+    }catch(err){
+      alert(err);
+      countTable.innerHTML = listCount.rows.length;
+    }
+    clearInterval(countTime);
   }
-  clearInterval(countTime);
 }
 
 function findName(targetNum) {
@@ -180,11 +184,7 @@ function serarchFetch(myJson, val){
     if (myJson.length - 1 === i) {
       targetCount.value = Number(myJson[i].id) + 1;
     }
-      if(searchType.value === ""){
-        alert("분류를 선택하세요");
-        getData();
-        return;
-      }else if(searchType.value === "name"){
+      if(searchType.value === "name"){
         if(myJson[i].name.includes(val)){
           const name = myJson[i].name.replace(val,`<span style="color: blue;">${val}</span>`)
           targetList.innerHTML += `<tr><td data-index=${myJson[i].id}>${k}</td><td class="target-name${i}"><span class="view-data">${name}</span><input class="correction-input" id="up-name" type="text" readonly value="${myJson[i].name}"></td><td><span class="view-data">${myJson[i].age}</span><input class="correction-input" id="up-age" type="text" readonly value="${myJson[i].age}"></td><td><span class="view-data">${myJson[i].job}</span><input class="correction-input" id="up-job" type="text" readonly value="${myJson[i].job}"></td><td><div class="button-box"><span><span class="view-data">${myJson[i].email}</span><input class="correction-input" id="up-email" type="text" readonly value="${myJson[i].email}"></span><div><button class="correction-data" type="button">수정</button><button class="up-data" type="button">완료</button><button onclick="findName(${i});" type="button" class="del-btn">삭제</button></div></div></td></tr>`;
