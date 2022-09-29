3d2fd8c33397580cb0a028b2623981e164c44fe0
PC <kim000183@naver.com>
2022-09-29 22:18:21 +0900

    리팩토링 수정

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-const requestURL = 'http://localhost:3000/accoounts'
-const requestTimeurl = 'http://localhost:3000/businessHours'
+const requestURL = 'http://localhost:3000/accoounts';
+const requestTimeURL = 'http://localhost:3000/businessHours';
 const targetList = document.getElementById('target');
-const tragetName = document.getElementById('name')
-const targetJob = document.getElementById('job')
-const tragetAge = document.getElementById('age')
-const tragetEmail = document.getElementById('email')
+const tragetName = document.getElementById('name');
+const targetJob = document.getElementById('job');
+const tragetAge = document.getElementById('age');
+const tragetEmail = document.getElementById('email');
 const delList = document.getElementById('del-pop');
 const targetCount = document.getElementById('num');
 const countTable = document.getElementById('count-list');
 const listCount = document.getElementById('table-count');
 const searchInput = document.getElementById('search');
 const searchType = document.getElementById('search-type');
-const list = document.getElementById('target');
 const findOpen = document.getElementById('open-time');
 const findClose = document.getElementById('close-time');
 const TIME_OUT = 5000;
 const PAGE_COUNT = 15;
 
 async function fetchTimeout(resource, options = {}) {
-  const { timeout = TIME_OUT } = options;
-  
+  const { timeout = TIME_OUT } = options;  
   const controller = new AbortController();
   const id = setTimeout(() => controller.abort(), timeout);
   const response = await fetch(resource, {
@@ -30,7 +28,7 @@ async function fetchTimeout(resource, options = {}) {
   return response;
 }
 
-async function loadGames() {
+async function loadTime() {
   try {
     const response = await fetchTimeout(requestURL, {
       timeout: TIME_OUT
@@ -43,12 +41,12 @@ async function loadGames() {
 }
 
 async function getTime(){
-  try{
-    const data = await fetchOption(requestTimeurl);
-    loadGames();
+  try {
+    const data = await fetchOption(requestTimeURL);
+    loadTime();
     const post = await data.json()
     await viewTime(post);
-  }catch(err){
+  } catch (err){
     alert(err)
   }
 }
@@ -61,12 +59,12 @@ function viewTime(myJson){
 getTime();
 
 async function getData() {
-  try{
+  try {
     const data = await fetchOption(requestURL);
-    loadGames();
+    loadTime();
     const post = await data.json()
     await setUserName(post)
-  }catch(err){
+  } catch (err){
       alert(err);
   }
 }
@@ -74,11 +72,11 @@ async function getData() {
 async function postData(event) {
   if (event.target.getAttribute('class') === 'submit-btn') {
     const email = tragetEmail.value;
-    if(!emailCheck(email)){
+    if (!emailCheck(email)){
       alert('email을 형식에 맞게 입력하세요.');
-    }else{
-      if(listCount.rows.length < PAGE_COUNT){
-        try{
+    } else {
+      if (listCount.rows.length < PAGE_COUNT){
+        try {
           await fetch(requestURL, {
             method: "POST",
             headers: {
@@ -92,31 +90,31 @@ async function postData(event) {
               email : tragetEmail.value
             }),
           })
-          loadGames();
+          loadTime();
           resetView();
-        }catch(err){
+        } catch (err){
           alert(err);
         }
-      }else{
+      } else {
         alert(`회원이 ${PAGE_COUNT}명 이상입니다.`);
       }
     }
   } else if (event.target.id === 'search-btn') {
     showList(searchInput.value)
-  }else if(event.target.getAttribute('class') === 'correction-data'){
+  } else if (event.target.getAttribute('class') === 'correction-data'){
     event.target.style.display = "none";
     event.target.nextSibling.style.display = "block";
     const updateTr = event.target.parentNode.parentNode.parentNode.parentNode; 
     const updateInput = updateTr.querySelectorAll('input');
     const noneSpan = updateTr.querySelectorAll('.view-data');
-    for(let i = 0; i < updateInput.length; i++){
+    for (let i = 0; i < updateInput.length; i++){
       noneSpan[i].style.display = "none"
       updateInput[i].style.display = "inline-block"
       updateInput[i].readOnly  = false;
       updateInput[i].style.border ='1px solid #888',
       updateInput[i].style.background ='#fff'
     }
-  }else if(event.target.getAttribute('class') === 'up-data'){
+  } else if (event.target.getAttribute('class') === 'up-data'){
     const updateTr = event.target.parentNode.parentNode.parentNode.parentNode;
     const patchNum = updateTr.firstChild.dataset.index;
     const upName = updateTr.querySelector('#up-name');
@@ -125,7 +123,7 @@ async function postData(event) {
     const upemail = updateTr.querySelector('#up-email');
     event.target.style.display = "none";
     event.target.previousSibling .style.display = "block";
-    try{
+    try {
       await fetch(`${requestURL}/${Number(patchNum)}`, {
         method: "PUT",
         headers: {
@@ -139,13 +137,13 @@ async function postData(event) {
           email : upemail.value
         }),
       })
-      loadGames();
+      loadTime();
       resetView();
       alert("수정이 완료되었습니다.");
-    }catch(err){
+    } catch (err){
       alert(err);
     }
-  }else if(event.target.id === 'reset-btn'){
+  } else if (event.target.id === 'reset-btn'){
     resetView();
     searchInput.value = '';
   }
@@ -153,28 +151,28 @@ async function postData(event) {
 
 async function deleteData(num) {
   delList.style.display = 'none';
-  try{
+  try {
     await fetch(`${requestURL}/${num}`, {
       method: "DELETE",
     })
-    loadGames();
+    loadTime();
     resetView();
-  }catch(err){
+  } catch (err){
     alert(err);
   }
 }
 
 async function showList(val) {
-  if(searchType.value === '' || searchType.value === '선택'){
+  if (searchType.value === '' || searchType.value === '선택'){
     alert("분류를 선택하세요"); 
-  }else{
-    list.innerHTML = '';
-    try{
+  } else {
+    targetList.innerHTML = '';
+    try {
       const data = await fetchOption(requestURL);
-      loadGames();
+      loadTime();
       const post = await data.json()
       await serarchFetch(post, val);
-    }catch(err){
+    } catch (err){
       alert(err);
       countTable.innerHTML = listCount.rows.length;
     }
@@ -199,34 +197,34 @@ function serarchFetch(myJson, val){
     if (myJson.length - 1 === i) {
       targetCount.value = Number(myJson[i].id) + 1;
     }
-    if(searchType.value === "name"){
-      if(myJson[i].name.includes(val)){
+    if (searchType.value === "name"){
+      if (myJson[i].name.includes(val)){
         const name = myJson[i].name.replace(val,`<span style="color: blue;">${val}</span>`)
         targetList.innerHTML += `<tr><td data-index=${myJson[i].id}>${k}</td><td class="target-name${i}"><span class="view-data">${name}</span><input class="correction-input" id="up-name" type="text" readonly value="${myJson[i].name}"></td><td><span class="view-data">${myJson[i].age}</span><input class="correction-input" id="up-age" type="text" readonly value="${myJson[i].age}"></td><td><span class="view-data">${myJson[i].job}</span><input class="correction-input" id="up-job" type="text" readonly value="${myJson[i].job}"></td><td><div class="button-box"><span><span class="view-data">${myJson[i].email}</span><input class="correction-input" id="up-email" type="text" readonly value="${myJson[i].email}"></span><div><button class="correction-data" type="button">수정</button><button class="up-data" type="button">완료</button><button onclick="findName(${i});" type="button" class="del-btn">삭제</button></div></div></td></tr>`;
         k++;
       }
-    }else if(searchType.value === "email"){
-      if(myJson[i].email.includes(val)){
+    } else if (searchType.value === "email"){
+      if (myJson[i].email.includes(val)){
         const email = myJson[i].email.replace(val,`<span style="color: blue;">${val}</span>`)
         targetList.innerHTML +=  `<tr><td data-index=${myJson[i].id}>${k}</td><td class="target-name${i}"><span class="view-data">${myJson[i].name}</span><input class="correction-input" id="up-name" type="text" readonly value="${myJson[i].name}"></td><td><span class="view-data">${myJson[i].age}</span><input class="correction-input" id="up-age" type="text" readonly value="${myJson[i].age}"></td><td><span class="view-data">${myJson[i].job}</span><input class="correction-input" id="up-job" type="text" readonly value="${myJson[i].job}"></td><td><div class="button-box"><span><span class="view-data">${email}</span><input class="correction-input" id="up-email" type="text" readonly value="${myJson[i].email}"></span><div><button class="correction-data" type="button">수정</button><button class="up-data" type="button">완료</button><button onclick="findName(${i});" type="button" class="del-btn">삭제</button></div></div></td></tr>`;
         k++;
       }
-    }else if(searchType.value === "age"){
-      if(myJson[i].age === Number(val)){
+    } else if (searchType.value === "age"){
+      if (myJson[i].age === Number(val)){
         const stringVal = val.toString();
         const age = myJson[i].age.toString().replace(stringVal,`<span style="color: blue;">${stringVal}</span>`)
         targetList.innerHTML += `<tr><td data-index=${myJson[i].id}>${k}</td><td class="target-name${i}"><span class="view-data">${myJson[i].name}</span><input class="correction-input" id="up-name" type="text" readonly value="${myJson[i].name}"></td><td><span class="view-data">${age}</span><input class="correction-input" id="up-age" type="text" readonly value="${myJson[i].age}"></td><td><span class="view-data">${myJson[i].job}</span><input class="correction-input" id="up-job" type="text" readonly value="${myJson[i].job}"></td><td><div class="button-box"><span><span class="view-data">${myJson[i].email}</span><input class="correction-input" id="up-email" type="text" readonly value="${myJson[i].email}"></span><div><button class="correction-data" type="button">수정</button><button class="up-data" type="button">완료</button><button onclick="findName(${i});" type="button" class="del-btn">삭제</button></div></div></td></tr>`;
         k++;
       }
-    }else if(searchType.value === "job"){
-      if(myJson[i].job === val){
+    } else if (searchType.value === "job"){
+      if (myJson[i].job === val){
         const job = myJson[i].job.replace(val,`<span style="color: blue;">${val}</span>`)
         targetList.innerHTML += `<tr><td data-index=${myJson[i].id}>${k}</td><td class="target-name${i}"><span class="view-data">${myJson[i].name}</span><input class="correction-input" id="up-name" type="text" readonly value="${myJson[i].name}"></td><td><span class="view-data">${myJson[i].age}</span><input class="correction-input" id="up-age" type="text" readonly value="${myJson[i].age}"></td><td><span class="view-data">${job}</span><input class="correction-input" id="up-job" type="text" readonly value="${myJson[i].job}"></td><td><div class="button-box"><span><span class="view-data">${myJson[i].email}</span><input class="correction-input" id="up-email" type="text" readonly value="${myJson[i].email}"></span><div><button class="correction-data" type="button">수정</button><button class="up-data" type="button">완료</button><button onclick="findName(${i});" type="button" class="del-btn">삭제</button></div></div></td></tr>`;
         k++;
       }
     }
   }
-  if(listCount.rows.length === 0){
+  if (listCount.rows.length === 0){
     throw new Error('회원이 존재하지 않습니다.');
   }
   countTable.innerHTML = listCount.rows.length;
@@ -251,12 +249,12 @@ function setUserName(myJson){
 }
 
 function showValue(target) {
-    searchType.value = target.value;
+  searchType.value = target.value;
 }
 
 function emailCheck(email) {
   const regex = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-  return (email != '' && email != 'undefined' && regex.test(email));
+  return (email !== '' && email !== 'undefined' && regex.test(email));
 }
 
 function resetView(){
@@ -265,4 +263,6 @@ function resetView(){
 }
 
 document.addEventListener('click', postData)
+
+
 
