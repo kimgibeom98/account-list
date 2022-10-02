412bf555d58dc2d0651141c6468039a623030ac3
PC <kim000183@naver.com>
2022-10-02 15:31:27 +0900

    함수명 수정

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function fetchTimeout(resource, options = {}) {
 
 async function loadTime() {
   try {
+    
     const response = await fetchTimeout(requestURL, {
       timeout: TIME_OUT
     });
@@ -91,7 +92,7 @@ async function postData(event) {
             }),
           })
           loadTime();
-          resetView();
+          render();
         } catch (err){
           alert(err);
         }
@@ -138,13 +139,13 @@ async function postData(event) {
         }),
       })
       loadTime();
-      resetView();
+      render();
       alert("수정이 완료되었습니다.");
     } catch (err) {
       alert(err);
     }
   } else if (event.target.id === 'reset-btn') {
-    resetView();
+    render();
     searchInput.value = '';
   }
 }
@@ -156,7 +157,7 @@ async function deleteData(num) {
       method: "DELETE",
     })
     loadTime();
-    resetView();
+    render();
   } catch (err) {
     alert(err);
   }
@@ -197,27 +198,21 @@ function searchResult(myJson, val){
     if (myJson.length - 1 === i) {
       targetCount.value = Number(myJson[i].id) + 1;
     }
-    if (searchType.value === "name") {
-      if (myJson[i].name.includes(val)) {
+    if (searchType.value === "name" || searchType.value === "email" || searchType.value === "age" || searchType.value === "job") {
+      if (searchType.value === "name" && myJson[i].name.includes(val)) {
         const name = myJson[i].name.replace(val,`<span style="color: blue;">${val}</span>`)
         targetList.innerHTML += `<tr><td data-index=${myJson[i].id}>${k}</td><td class="target-name${i}"><span class="view-data">${name}</span><input class="correction-input" id="up-name" type="text" readonly value="${myJson[i].name}"></td><td><span class="view-data">${myJson[i].age}</span><input class="correction-input" id="up-age" type="text" readonly value="${myJson[i].age}"></td><td><span class="view-data">${myJson[i].job}</span><input class="correction-input" id="up-job" type="text" readonly value="${myJson[i].job}"></td><td><div class="button-box"><span><span class="view-data">${myJson[i].email}</span><input class="correction-input" id="up-email" type="text" readonly value="${myJson[i].email}"></span><div><button class="correction-data" type="button">수정</button><button class="up-data" type="button">완료</button><button onclick="findName(${i});" type="button" class="del-btn">삭제</button></div></div></td></tr>`;
         k++;
-      }
-    } else if (searchType.value === "email") {
-      if (myJson[i].email.includes(val)) {
+      } else if (searchType.value === "email" && myJson[i].email.includes(val)) {
         const email = myJson[i].email.replace(val,`<span style="color: blue;">${val}</span>`)
         targetList.innerHTML +=  `<tr><td data-index=${myJson[i].id}>${k}</td><td class="target-name${i}"><span class="view-data">${myJson[i].name}</span><input class="correction-input" id="up-name" type="text" readonly value="${myJson[i].name}"></td><td><span class="view-data">${myJson[i].age}</span><input class="correction-input" id="up-age" type="text" readonly value="${myJson[i].age}"></td><td><span class="view-data">${myJson[i].job}</span><input class="correction-input" id="up-job" type="text" readonly value="${myJson[i].job}"></td><td><div class="button-box"><span><span class="view-data">${email}</span><input class="correction-input" id="up-email" type="text" readonly value="${myJson[i].email}"></span><div><button class="correction-data" type="button">수정</button><button class="up-data" type="button">완료</button><button onclick="findName(${i});" type="button" class="del-btn">삭제</button></div></div></td></tr>`;
         k++;
-      }
-    } else if (searchType.value === "age") {
-      if (myJson[i].age === Number(val)) {
+      } else if (searchType.value === "age" && myJson[i].age === Number(val)) {
         const stringVal = val.toString();
         const age = myJson[i].age.toString().replace(stringVal,`<span style="color: blue;">${stringVal}</span>`)
         targetList.innerHTML += `<tr><td data-index=${myJson[i].id}>${k}</td><td class="target-name${i}"><span class="view-data">${myJson[i].name}</span><input class="correction-input" id="up-name" type="text" readonly value="${myJson[i].name}"></td><td><span class="view-data">${age}</span><input class="correction-input" id="up-age" type="text" readonly value="${myJson[i].age}"></td><td><span class="view-data">${myJson[i].job}</span><input class="correction-input" id="up-job" type="text" readonly value="${myJson[i].job}"></td><td><div class="button-box"><span><span class="view-data">${myJson[i].email}</span><input class="correction-input" id="up-email" type="text" readonly value="${myJson[i].email}"></span><div><button class="correction-data" type="button">수정</button><button class="up-data" type="button">완료</button><button onclick="findName(${i});" type="button" class="del-btn">삭제</button></div></div></td></tr>`;
         k++;
-      }
-    } else if (searchType.value === "job") {
-      if (myJson[i].job === val) {
+      } else if (searchType.value === "job" && myJson[i].job === val) {
         const job = myJson[i].job.replace(val,`<span style="color: blue;">${val}</span>`)
         targetList.innerHTML += `<tr><td data-index=${myJson[i].id}>${k}</td><td class="target-name${i}"><span class="view-data">${myJson[i].name}</span><input class="correction-input" id="up-name" type="text" readonly value="${myJson[i].name}"></td><td><span class="view-data">${myJson[i].age}</span><input class="correction-input" id="up-age" type="text" readonly value="${myJson[i].age}"></td><td><span class="view-data">${job}</span><input class="correction-input" id="up-job" type="text" readonly value="${myJson[i].job}"></td><td><div class="button-box"><span><span class="view-data">${myJson[i].email}</span><input class="correction-input" id="up-email" type="text" readonly value="${myJson[i].email}"></span><div><button class="correction-data" type="button">수정</button><button class="up-data" type="button">완료</button><button onclick="findName(${i});" type="button" class="del-btn">삭제</button></div></div></td></tr>`;
         k++;
@@ -257,7 +252,7 @@ function emailCheck(email) {
   return (email !== '' && email !== 'undefined' && regex.test(email));
 }
 
-function resetView(){
+function render(){
   targetList.innerHTML = '';
   getData();
 }
